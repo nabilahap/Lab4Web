1be8dc1617b59b8058051b471e0faa1bb79a2e0d
nabilahap <nabilaananda1325@gmail.com>
2023-04-04 13:15:04 +0700

    fist commit

---
+++ Lab4/about.php
@@ -0,0 +1,8 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>About</h2>
+    <div class="box">
+        <p> Terimakasih telah mengunjungi website kami, kami mengharapkan saran dan kritik anda.</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
---
+++ Lab4/kontak.php
@@ -0,0 +1,12 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>Kontak</h2>
+    <div class="box">
+        <p>Nama : Nabilah Ananda Putri</p>
+        <p>Nim : 312110263</p>
+        <p>Kelas : TI.21.A.1</p>
+        <p>Matkul : Pemograman Web 2</p>
+        <p>Email : nabilaananda1325@gmail.com</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
